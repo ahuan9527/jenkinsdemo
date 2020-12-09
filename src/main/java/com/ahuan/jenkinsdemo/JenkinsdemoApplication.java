2a3d49a1adc9c084e075cca832215d88ad38221d
ahuan9527 <1875884755@qq.com>
2020-12-09 17:21:34 +0800

    测试Jenkins自动部署打包

---
+++ src/main/java/com/ahuan/jenkinsdemo/JenkinsdemoApplication.java
@@ -0,0 +1,16 @@
+package com.ahuan.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ahuan
+ */
+@SpringBootApplication
+public class JenkinsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsdemoApplication.class, args);
+    }
+
+}
