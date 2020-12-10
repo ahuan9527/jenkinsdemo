2d6dd20b616ef317395c2c4507dad005430ea7c8
ahuan9527 <1875884755@qq.com>
2020-12-10 15:44:01 +0800

    增加测试接口！

---
+++ src/main/java/com/ahuan/jenkinsdemo/controller/Hello.java
@@ -0,0 +1,24 @@
+package com.ahuan.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description <What does this class do>
+ * @DATE 2020/12/10 15:39
+ * @Author ahuan
+ * @ProjectName jenkinsdemo
+ * @Email a1875884755@gmail.com
+ * @Version 1.0
+ */
+@RestController("/hello")
+public class Hello {
+
+    @RequestMapping(value = "/oo",method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(){
+        return "你好啊，我是HELLO WORLD!";
+    }
+}
